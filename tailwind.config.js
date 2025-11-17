d35e23e51a14b940e5606d5bab98f141af116f84
enderjpinar.dev <enderjpinar.dev@gmail.com>
2025-11-17 11:39:42 -0300

    mejoras en clusters

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./api/**/*.{js,ts,jsx,tsx}",
+    "./scripts/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
